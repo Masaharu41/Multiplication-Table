61313723e067b9f4340049ad46c8ed8bb3a59cc6
Owen Fujii <dalekrebel@gmail.com>
2024-01-25 08:09:41 -0700

    Add project files.

---
+++ MultiplicationTable.vb
@@ -0,0 +1,16 @@
+﻿'Owen Fujii
+'RCET 2265
+'Spring 2024
+'Multiplication Table
+'
+
+Option Explicit On
+Option Strict On
+
+Module MultiplicationTable
+
+    Sub Main()
+
+    End Sub
+
+End Module
